7c54be322658f44067303a92640969eebbc8c49f
Aries <hutao19930614@foxmail.com>
2017-08-17 22:51:46 +0800

    1、新增底部输入框处理方案 2、调整检测不同手机系统版本工具类处理

--- app/src/main/java/com/aries/ui/widget/demo/module/title/TitleActivity.java
+++ app/src/main/java/com/aries/ui/widget/demo/module/title/TitleActivity.java
@@ -26,6 +26,7 @@ import com.aries.ui.widget.demo.helper.DrawerHelper;
 import com.aries.ui.widget.demo.util.ViewUtil;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.sunfusheng.glideimageview.GlideImageView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,6 +45,7 @@ public class TitleActivity extends BaseRecycleActivity<TitleEntity> {
     @BindView(R.id.titleBarDrawer) TitleBarView titleBarDrawer;
     @BindView(R.id.fLayout_drawer) FrameLayout fLayoutDrawer;
     @BindView(R.id.rv_contentDrawer) RecyclerView mRecyclerViewDrawer;
+    @BindView(R.id.giv_headDrawer) GlideImageView givHead;
     private SwitchCompat sBtnImmersible;
     private SwitchCompat sBtnLight;
     private SwitchCompat sBtnLine;
@@ -110,6 +112,7 @@ public class TitleActivity extends BaseRecycleActivity<TitleEntity> {
     @Override
     protected void initView(Bundle bundle) {
         super.initView(bundle);
+        givHead.loadCircleImage("https://avatars3.githubusercontent.com/u/19605922?v=4&s=460",android.R.color.holo_purple);
         titleBarDrawer.setImmersible(mContext, isImmersible, isLight);
         vHeader = View.inflate(mContext, R.layout.layout_title_header, null);
         sBtnImmersible = (SwitchCompat) vHeader.findViewById(R.id.sBtn_immersible);
